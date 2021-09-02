58c3ef7d1ef8fc0fd7798b0d90d252a68577aed5
Abdul Moiz <moiza8994@gmail.com>
2021-09-02 14:48:03 +0500

    add the serverisde index.js pratcie

--- Server_side/index.js
+++ Server_side/index.js
@@ -1,64 +1,75 @@
-const { ApolloServer, gql } = require("apollo-server");
 
-const Students = [
+
+const {ApolloServer, gql} =require("apollo-server")
+
+const Contact=[
   {
     id: 1,
-    name: "Abdul Moiz",
-    email: "moiza8994@gmail.com",
-    number: 111111111,
-    age: 20,
-    isPresent: "Access"
+    firstName: "Abdul",
+     lastName: "Moiz",
+     email: "moiza8994@gmail.com",
+     PhoneNumber: 11111111,
+     age: 20
   },
   {
     id: 2,
-    name: "Abdullah",
-    email: "abdullah8994@gmail.com",
-    number: 222222222,
-    age: 22,
+    firstName: "Abdul",
+    lastName: "Rafay",
+    email: "rafaya8994@gmail.com",
+    PhoneNumber: 22222222,
+    age: 22
   },
   {
     id: 3,
-    name: "Rizwan",
-    email: "rizwan8994@gmail.com",
-    number: 333333333,
-    age: 24,
+    firstName: "Abdul",
+    lastName: "Malik",
+    email: "malik8994@gmail.com",
+    PhoneNumber: 33333333,
+    age: 22
   },
   {
     id: 4,
-    name: "Faizan",
-    email: "faizan8994@gmail.com",
-    number: 444444444,
-    age: 18,
+    firstName: "Abdul",
+     lastName: "Moid",
+     email: "moida8994@gmail.com",
+     PhoneNumber: 444444444,
+     age: 20
   },
-];
-
-const resolvers = {
-  Query: {
-    students: () => {
-      return Students
-    },
-  },
-};
-
-
-// We make the Types first word Capital
-const typeDefs = gql`
-  type Student {
-    id: Int
-    name: String
-    email: String
-    number: Int
-    age: Int
-    isPresent: String
+  {
+    id: 5,
+    firstName: "Abdullah",
+     lastName: "khan",
+     email: "abdullaha8994@gmail.com",
+     PhoneNumber: 555555555,
+     age: 22
   }
+]
 
-  type Query {
-    students: [Student]
-  }
-`;
 
-const server = new ApolloServer({ typeDefs, resolvers });
-// the listen method launches a web server
-server.listen().then(({ url }) => {
-  console.log(` Server is ready ${url}`);
-});
+const resolvers={
+    Query:{
+      contactLiwsts: () => Contact
+    }
+}
+
+
+
+
+const  typeDefs = gql`
+   type List{
+     id: Int
+     firstName: String
+     lastName: String
+     email: String
+     PhoneNumber: Int
+     age: Int
+   }
+  type Query{
+     contactLists: [List]
+  }`
+
+const server= new ApolloServer({typeDefs ,resolvers});
+server.listen().then(({url}) =>{
+     console.log(`🚀  Server ready at ${url}`);
+})
+
