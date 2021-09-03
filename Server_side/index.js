b8e51cc85487643163de3d27fc190f579987b3f1
Abdul Moiz <moiza8994@gmail.com>
2021-09-03 11:20:25 +0500

    add the chnages in index.js server side file

--- Server_side/index.js
+++ Server_side/index.js
@@ -1,15 +1,13 @@
+const { ApolloServer, gql } = require("apollo-server");
 
-
-const {ApolloServer, gql} =require("apollo-server")
-
-const Contact=[
+const Contact = [
   {
     id: 1,
     firstName: "Abdul",
-     lastName: "Moiz",
-     email: "moiza8994@gmail.com",
-     PhoneNumber: 11111111,
-     age: 20
+    lastName: "Moiz",
+    email: "moiza8994@gmail.com",
+    PhoneNumber: 11111111,
+    age: 20,
   },
   {
     id: 2,
@@ -17,7 +15,7 @@ const Contact=[
     lastName: "Rafay",
     email: "rafaya8994@gmail.com",
     PhoneNumber: 22222222,
-    age: 22
+    age: 22,
   },
   {
     id: 3,
@@ -25,51 +23,47 @@ const Contact=[
     lastName: "Malik",
     email: "malik8994@gmail.com",
     PhoneNumber: 33333333,
-    age: 22
+    age: 22,
   },
   {
     id: 4,
     firstName: "Abdul",
-     lastName: "Moid",
-     email: "moida8994@gmail.com",
-     PhoneNumber: 444444444,
-     age: 20
+    lastName: "Moid",
+    email: "moida8994@gmail.com",
+    PhoneNumber: 444444444,
+    age: 20,
   },
   {
     id: 5,
     firstName: "Abdullah",
-     lastName: "khan",
-     email: "abdullaha8994@gmail.com",
-     PhoneNumber: 555555555,
-     age: 22
+    lastName: "khan",
+    email: "abdullaha8994@gmail.com",
+    PhoneNumber: 555555555,
+    age: 22,
+  },
+];
+
+const resolvers = {
+  Query: {
+    contactLiwsts: () => Contact,
+  },
+};
+
+const typeDefs = gql`
+  type List {
+    id: Int
+    firstName: String
+    lastName: String
+    email: String
+    PhoneNumber: Int
+    age: Int
   }
-]
+  type Query {
+    contactLists: [List]
+  }
+`;
 
-
-const resolvers={
-    Query:{
-      contactLiwsts: () => Contact
-    }
-}
-
-
-
-
-const  typeDefs = gql`
-   type List{
-     id: Int
-     firstName: String
-     lastName: String
-     email: String
-     PhoneNumber: Int
-     age: Int
-   }
-  type Query{
-     contactLists: [List]
-  }`
-
-const server= new ApolloServer({typeDefs ,resolvers});
-server.listen().then(({url}) =>{
-     console.log(`🚀  Server ready at ${url}`);
-})
-
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
