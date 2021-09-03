bd016ebe8fffeced37429e174fd2f310f9d5d627
Abdul Moiz <moiza8994@gmail.com>
2021-09-03 12:12:16 +0500

    add the server side practice of index graphQl with apollo

--- Server_side/index.js
+++ Server_side/index.js
@@ -1,69 +1,64 @@
-const { ApolloServer, gql } = require("apollo-server");
+const {ApolloServer,gql}= require('apollo-server');
 
-const Contact = [
-  {
-    id: 1,
-    firstName: "Abdul",
-    lastName: "Moiz",
-    email: "moiza8994@gmail.com",
-    PhoneNumber: 11111111,
-    age: 20,
-  },
-  {
-    id: 2,
-    firstName: "Abdul",
-    lastName: "Rafay",
-    email: "rafaya8994@gmail.com",
-    PhoneNumber: 22222222,
-    age: 22,
-  },
-  {
-    id: 3,
-    firstName: "Abdul",
-    lastName: "Malik",
-    email: "malik8994@gmail.com",
-    PhoneNumber: 33333333,
-    age: 22,
-  },
-  {
-    id: 4,
-    firstName: "Abdul",
-    lastName: "Moid",
-    email: "moida8994@gmail.com",
-    PhoneNumber: 444444444,
-    age: 20,
-  },
-  {
-    id: 5,
-    firstName: "Abdullah",
-    lastName: "khan",
-    email: "abdullaha8994@gmail.com",
-    PhoneNumber: 555555555,
-    age: 22,
-  },
-];
 
-const resolvers = {
+
+const contactData=[
+  {
+    fullName: "ABdul Moiz",
+    fatherName: "zafar Ali",
+    age: 20,
+    phoneNumber: 11111111,
+    email: "moiza8994@gmail.com"
+  },
+  {
+    fullName: "ABdul Moiz",
+    fatherName: "zafar Ali",
+    age: 20,
+    phoneNumber: 11111111,
+    email: "moiza8994@gmail.com"
+  },
+  {
+    fullName: "ABdul Moiz",
+    fatherName: "zafar Ali",
+    age: 20,
+    phoneNumber: 11111111,
+    email: "moiza8994@gmail.com"
+  },
+  {
+    fullName: "ABdul Moiz",
+    fatherName: "zafar Ali",
+    age: 20,
+    phoneNumber: 11111111,
+    email: "moiza8994@gmail.com"
+  },
+]
+
+
+const resolvers={
   Query: {
-    contactLiwsts: () => Contact,
-  },
-};
-
-const typeDefs = gql`
-  type List {
-    id: Int
-    firstName: String
-    lastName: String
-    email: String
-    PhoneNumber: Int
-    age: Int
+    contacts: () => {
+      return contactData
+    }
   }
-  type Query {
-    contactLists: [List]
-  }
-`;
+}
 
-const server = new ApolloServer({ typeDefs, resolvers });
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+
+
+// make the schema
+const typeDefs=gql`
+    type contact{
+      fullName: String
+      fatherName: String
+      age: Int
+      phoneNumber: Int
+      email: String
+    }
+  type Query{
+    contacts: [contact]
+  }`
+
+  const server= new ApolloServer({typeDefs, resolvers})
+
+  server.listen().then(({url}) =>{
+       console.log(` 🚀  Server ready at ${url}`);    
+  })
