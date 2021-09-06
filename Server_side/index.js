a1f48e7cf6c6c7b44ea4f324c09258216c034fdb
Abdul Moiz <moiza8994@gmail.com>
2021-09-06 12:43:10 +0500

    add the index.js another request and type of course

--- Server_side/index.js
+++ Server_side/index.js
@@ -31,16 +31,35 @@ const contactData=[
     phoneNumber: 11111111,
     email: "moiza8994@gmail.com"
   },
-]
+];
 
+const course=[ 
+
+  {
+    courseId: 1,
+    courseName: "Cloud Computing",
+    courseToken: 000011,
+  },
+
+  {
+    courseId:2,
+    courseName: "Blockchain",
+    courseToken: 000022,
+  }
+];
 
 const resolvers={
   Query: {
     contacts: () => {
       return contactData
-    }
+    },
+    course: () =>{
+      return course
+    }    
+
   }
 }
+
 
 
 
@@ -55,12 +74,20 @@ const typeDefs=gql`
       email: String
     }
 
+     type Course{
+       courseId: Int
+       courseName: String
+       courseToken: Int
+     }
+
+
   type Query{
     contacts: [contact]
+    course: [Course]
   }`
 
   const server= new ApolloServer({typeDefs, resolvers})
 
   server.listen().then(({url}) =>{
-       console.log(` 🚀  Server ready at ${url}`);    
+       console.log(` 🚀  Server ready at the ${url}`);    
   })
